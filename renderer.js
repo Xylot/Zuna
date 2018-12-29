282e841b54c560446d909c2fb199076342a59d33
Xylot <Joseph.Meriam@gmail.com>
2018-12-29 02:48:30 -0600

    Ability to populate all replay information

--- renderer.js
+++ renderer.js
@@ -6,32 +6,61 @@ const dtbs = require('datatables.net-bs4')(window, $);
 
 
 function getReplayJson(callback) {
-    $.getJSON("data.json", function(json) {
+    $.getJSON("data2.json", function(json) {
         callback(json)
     });
 }
 
+var dataSet2 = []
+
 getReplayJson(function(json) {
-    buildID = json.Build_ID;
-    buildVersion = json.Build_Verison;
-    gameVersion = json.Game_Version;
-    replayVersion = json.Replay_Version;
-    date = json.Date;
-    id = json.ID;
-    matchType = json.Match_Type;
-    teamSize = json.Team_Size;
-    map = json.Map;
-    goalsOrange = json.Goals_Orange;
-    goalsBlue = json.Goals_Blue;
-    user = json.User;
-    players = json.Players;
-    headerProps = [
-        buildID, buildVersion, gameVersion,
-        replayVersion, date, id,
-        matchType, teamSize, map,
-        goalsOrange, goalsBlue, user,
-        players
-    ];
+
+    var index = [];
+
+    // build the index
+    for (var x in json) {
+       index.push(x);
+    }
+    
+    // sort the index
+    index.sort(function (a, b) {    
+       return a == b ? 0 : (a > b ? 1 : -1); 
+    }); 
+
+    var count = Object.keys(json).length;
+
+    replayData = []
+    for(i = 0; i < count; i++) {
+        currentJSON = json[index[i]];
+        buildID = currentJSON.Build_ID;
+        buildVersion = currentJSON.Build_Verison;
+        gameVersion = currentJSON.Game_Version;
+        replayVersion = currentJSON.Replay_Version;
+        date = currentJSON.Date;
+        id = currentJSON.ID;
+        matchType = currentJSON.Match_Type;
+        teamSize = currentJSON.Team_Size;
+        map = currentJSON.Map;
+        goalsOrange = currentJSON.Goals_Orange;
+        goalsBlue = currentJSON.Goals_Blue;
+        user = currentJSON.User;
+        players = currentJSON.Players;
+        replayName = currentJSON.Replay_Name;
+        headerProps = [
+            buildID, buildVersion, gameVersion,
+            replayVersion, date, id,
+            matchType, teamSize, map,
+            goalsOrange, goalsBlue, user,
+            players, replayName
+        ];
+        replayData.push(headerProps);        
+        if (replayName !== null) {
+            dataSet2.push([replayName, date, teamSize, matchType, map, goalsOrange + ':' + goalsBlue])
+        }
+        else {
+            dataSet2.push([id, date, teamSize, matchType, map, goalsOrange + ':' + goalsBlue])
+        }
+    } 
 });
 
 var dataSet = [
@@ -80,12 +109,9 @@ var dataSet = [
 // Testing PowerShell
 $("#populate").click(() => {
 
-    // Get the form input or default to 'localhost'
-    let computer = $('#computerName').val() || 'localhost'
-
-    dataSet2 = [
-        [id, date, teamSize, matchType, map, goalsOrange + ':' + goalsBlue]
-    ];
+    // dataSet2 = [
+    //     [id, date, teamSize, matchType, map, goalsOrange + ':' + goalsBlue]
+    // ];
 
     // Create DataTable
     $('#output').DataTable({
@@ -101,43 +127,4 @@ $("#populate").click(() => {
             { title: "Goals (Orange:Blue)" }
         ]
     });
-})
-
-    // Create the PS Instance
-    let ps = new powershell({
-        executionPolicy: 'Bypass',
-        noProfile: true
-    })
-
-    // Load the gun
-    //ps.addCommand("\"Roads? Where we're going, we don't need roads.\"")
-    //ps.addCommand("Get-Process -Name electron")
-    
-    // Load the gun
-    // ps.addCommand("./Test-Power", [
-    //     { GigaWatts: 1.0 }
-    // ])
-
-    // Load the gun
-    ps.addCommand("./Get-Drives", [
-        { ComputerName: computer }
-    ])
-
-    // Pull the Trigger
-    ps.invoke()
-    .then(output => {
-        console.log(output)
-        let data = JSON.parse(output)
-        console.log(data)
-
-        // generate DataTables columns dynamically
-        let columns = [];
-        Object.keys(data[0]).forEach( key => columns.push({ title: key, data: key }) )
-
-        
-    .catch(err => {
-        console.error(err)
-        ps.dispose()
-    })
-
 })
